158ea0a8761aeae501afa1257cead7370cfc4776
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2025-11-08 23:30:21 +0100

    EX2 updated

--- Assets/Project/EX2/Scripts/Exercize2.cs
+++ Assets/Project/EX2/Scripts/Exercize2.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class Exercize2 : MonoBehaviour
 {
 
-   // da Fare
+    [SerializeField] private string STRINGA;
+     
 
+    // REGOLA PER LA STRINGA :
+    // (aragon;left;angry) Che siate attento, non è unu gingillo quello che portate!
+    // (gandalf;right) Uno stregone non è mai in ritardo, arriva precisamente quando intende farlo.
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Stampare singolarmente i contenuti della stringa
+
+        int indiceapertatonda = STRINGA.IndexOf("(");
+
+        int indicechiusatonda = STRINGA.IndexOf(")");
+
+        //string testotratonde = STRINGA.Substring(indiceapertatonda,)
+
+
+
+
     }
 
 }
---
+++ Assets/Project/EX3/Scripts/EX3.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EX3 : MonoBehaviour
+{
+    [SerializeField] private int NumeroMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //List<int> lista = new List<int>(NumeroMax);
+
+        //for (int i=0; i<NumeroMax; i++)
+        //{
+        //    if lista[i] = i;
+        //    lista
+        //}
+
+
+
+
+
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
